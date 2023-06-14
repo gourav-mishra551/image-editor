95467a84fecf4b9cdb6cb704eca0a6383a5b15eb
gourav-mishra551 <gouravmishra200105@gmail.com>
2023-06-14 22:11:22 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,24 @@
+const fileInput = document.querySelector(".file-input"),
+filterOptions = document.querySelectorAll(".filter button"),
+filterName = document.querySelector(".filter-info .name"),
+filterValue = document.querySelector(".filter-info .value"),
+filterSlider = document.querySelector(".slider input"),
+rotateOptions = document.querySelectorAll(".rotate button"),
+previewImg = document.querySelector(".preview-img img"),
+resetFilterBtn = document.querySelector(".reset-filter"),
+chooseImgBtn = document.querySelector(".choose-img"),
+saveImgBtn = document.querySelector(".save-img");
+ let brightness = '100' , saturation = '100' , inversion = "0",
+ grayscale = "0";
+ let rotate = 0, flipHorizontal = 1, flipvertical = 1;
+
+const loadImage = () => {
+    let file = fileInput.files[0];
+    if(!file) return;
+    previewImg.src = URL.createObjectURL(file);
+    previewImg.addEventListener("load", () => {
+        resetFilterBtn.click();
+        document.querySelector(".container").classList.remove
+        ("disable");
+    })
+}
